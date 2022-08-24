3a2b9a8347934fef2efc1aff6fe12be203a4fc49
ilanlavi <refaelberlin@gmail.com>
2022-08-25 01:22:54 +0200

    updating library

--- birthday-reminder/src/App.jsx
+++ birthday-reminder/src/App.jsx
@@ -4,28 +4,24 @@ import List from './list';
 
 function App() {
   const [people, setPeople] = useState(data);
-  const [counter, setCounter] = useState(data.length);
 
   const handleRemove = (id) => {
     const filterPeople = people.filter((person) => person.id !== id);
     setPeople(filterPeople);
-    setCounter((prevCount) => prevCount - 1);
   };
 
   const handleClear = () => {
     setPeople([]);
-    setCounter(0);
   };
 
   const handleReset = () => {
     setPeople(data);
-    setCounter(data.length);
   };
 
   return (
     <main>
       <section className='container'>
-        <h3>{`${counter} birthdays today`}</h3>
+        <h3>{`${people.length} birthdays today`}</h3>
         <List people={people} handleRemove={handleRemove} />
         <button onClick={handleClear}>Clear All</button>
         <button onClick={handleReset}>Reset</button>
