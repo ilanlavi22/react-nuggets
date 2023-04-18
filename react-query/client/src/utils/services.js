00ada4fe90de2eef7ed81f244fc7192a890f4087
ilanlavi <refaelberlin@gmail.com>
2023-04-18 21:55:21 +0200

    tasks json server env

--- react-query/client/src/utils/services.js
+++ react-query/client/src/utils/services.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5000/api/tasks' });
+//const api = axios.create({ baseURL: 'http://localhost:5000/api/tasks' });
+const api = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
 
 // export const fetchTasks = () => api.get('/').then((response) => response.data);
 
